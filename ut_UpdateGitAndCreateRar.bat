9c9a2b2bcfb79fd1623cf5add0b06342295905d8
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2024-06-24 22:04:29 -0300

    2024_06_24_220427Test6

--- ut_UpdateGitAndCreateRar.bat
+++ ut_UpdateGitAndCreateRar.bat
@@ -1,6 +1,6 @@
 @echo off
 @set NAMEBACKUP=Backup_%1
-@set datetimemf=%date:~-4%_%date:~3,2%_%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%_%1
+@set datetimemf=%date:~-4%_%date:~3,2%_%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 
 @atg -b:7 15 Backup
 @atg -b:7 15 Upload Git
