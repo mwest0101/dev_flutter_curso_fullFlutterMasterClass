1edaf884ad0c26dcba6593100f2fb524d7308a41
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2024-06-26 00:33:54 -0300

    2024_06_26_ 03350_V0005_3FramesVersion

--- lib/main.dart
+++ lib/main.dart
@@ -17,31 +17,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.deepPurple[2000],
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(20),
+        home: Scaffold(
+            backgroundColor: Colors.deepPurple[200],
+            appBar: AppBar(
+              title: const Text("My App Bar"),
+              backgroundColor: Colors.grey,
+              elevation: 0,
+              leading: const Icon(Icons.menu),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.logout),
+                ),
+              ],
             ),
-            padding: EdgeInsets.all(25),
-            // padding: EdgeInsets.symmetric(horizontal: 25, vertical: 50),
-            // padding: EdgeInsets.only(left: 25, top: 50),
-            child: const Text(
-              "Mitch K0K0",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+            body: Column(
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.deepPurple,
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.deepPurple[400],
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.deepPurple[200],
+                ),
+              ],
+            )));
   }
 }
