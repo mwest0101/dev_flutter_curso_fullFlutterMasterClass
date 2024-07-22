4313765eb6281e4e34f24bdc1b44278674899edf
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2024-07-22 00:18:27 -0300

    Se agrego boton TAp

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,26 @@
 import 'package:flutter/material.dart';
 
-import 'package:full_flutter_master_class/pages/first_page.dart';
-import 'package:full_flutter_master_class/pages/home_page.dart';
-import 'package:full_flutter_master_class/pages/settings_page.dart';
-
+import 'pages/todo_page.dart';
 /*
-https://www.youtube.com/watch?v=TclK5gNM_PM&t=3056s
-video:59:45
-
-
+video time
+1:05:14
 
 */
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
-  //functions & methods
-
-  List names = ["Mitch", "Sharon", "Vince"];
-  // void printNumbers() {}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: FirstPage(),
-        routes: {
-          '/firstpage': (context) => FirstPage(),
-          '/homepage': (context) => HomePage(),
-          '/settingspage': (context) => SettingsPage(),
-        });
+      debugShowCheckedModeBanner: false,
+      home: ToDoPage(),
+
+      
+    );
   }
 }
+
---
+++ lib/pages/todo_page.dart
@@ -0,0 +1,37 @@
+﻿import 'package:flutter/material.dart';
+
+class ToDoPage extends StatefulWidget {
+  const ToDoPage({super.key});
+
+  @override
+  State<ToDoPage> createState() => _ToDoPageState();
+}
+
+class _ToDoPageState extends State<ToDoPage> {
+  //  variable
+  TextEditingController myController =  TextEditingController();
+
+  void greetUser() { 
+    print(myController.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Column(
+          children:[
+            TextField(
+              controller: myController,
+            ),            
+            ElevatedButton(
+              onPressed: greetUser,
+              child:Text("Tap"),
+
+            ),
+          ],         
+        )
+      ),      
+    );
+  }
+}
